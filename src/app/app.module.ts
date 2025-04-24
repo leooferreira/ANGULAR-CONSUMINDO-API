6d8f89461a68d5e42e28ca908279dba4e4d9e61a
Leonardo Ferreira <leooferreira07@gmail.com>
2025-04-24 19:31:04 -0300

    updated angular to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { TopBarComponent } from './main/top-bar/top-bar.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    TopBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        TopBarComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule], providers: [
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
