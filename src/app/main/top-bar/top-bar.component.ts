dfc8f124bd6c2fdc884fded5227eb45ffaef4a4b
Leonardo Ferreira <leooferreira07@gmail.com>
2025-04-24 19:35:23 -0300

    updated to angular 19

--- src/app/main/top-bar/top-bar.component.ts
+++ src/app/main/top-bar/top-bar.component.ts
@@ -2,9 +2,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Music } from 'src/app/models/music.model';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.scss'],
+    standalone: false
 })
 export class TopBarComponent {
  @Input('music') musicaSelecionada!: Music; //recebendo dados do componente pai
